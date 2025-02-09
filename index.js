00734113840f0257f69165fe6a0ad21024dcf5d9
johnsnowai49 <johnsnowai49@gmail.com>
2025-02-10 00:04:56 +0800

    Update index page and add pictures

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ $(document).ready(function() {
     var envelope = $("#envelope");
     var btn_open = $("#open");
     var btn_reset = $("#reset");
-
+    var envelope_states = ['state-0', 'state-1', 'state-2'];
+    var envelope_state = 0
     envelope.click(function() {
-        open();
+        next_state();
     });
     btn_open.click(function() {
-        open();
+        next_state();
     });
     btn_reset.click(function() {
         close();
     });
-
+    function next_state() {
+        envelope.removeClass(envelope_states[envelope_state]);
+        envelope_state = (envelope_state + 1 ) % envelope_states.length;
+        envelope.addClass(envelope_states[envelope_state]);
+    }
     function open() {
         envelope.addClass("open")
             .removeClass("close");
